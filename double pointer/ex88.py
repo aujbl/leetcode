a3f9daea033f0d486a25d65a453fc2174fbe06ba
lee <aujblee@foxmail.com>
2021-06-24 17:22:18 +0800

    greedy

--- double pointer/ex88.py
+++ double pointer/ex88.py
@@ -10,26 +10,14 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        if not nums1:
-            return nums2
-        if not nums2:
-            return nums1
-        temp = nums1[0:m]
-        idx = i = j = 0
-        while idx < m+n:
-            if i == m:
-                nums1[idx:] = nums2[j::]
-                idx = m+n
-            elif j == n:
-                nums1[idx:] = temp[i:]
-                idx = m+n
-            else:    
-                if temp[i] <= nums2[j]:
-                    nums1[idx] = temp[i]
-                    i += 1
-                else:
-                    nums1[idx] = nums2[j]
-                    j += 1
-                idx += 1
+        i = j = 0
+        while i < m+j and j < n:
+            if nums1[i] >= nums2[j]:
+                nums1[i+1:m+1+j] = nums1[i:m+j]
+                nums1[i] = nums2[j]
+                j += 1
+            else:
+                i += 1
+        nums1[m+j:] = nums2[j:]
 
             
