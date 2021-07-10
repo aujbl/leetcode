c8818216e88d4a5acb4c80795d9092df610bad47
lee <aujblee@foxmail.com>
2021-07-10 09:18:17 +0800

    Boyer-Moore

--- ex17.10.py
+++ ex17.10.py
@@ -1,9 +1,26 @@
 from typing import List
 
 
-
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        candi, cnt = -1, 0
+        for num in nums:
+            if cnt == 0:
+                candi = num
+            if candi == num:
+                cnt += 1
+            else:
+                cnt -= 1
+        cnt = 0
+        for num in nums:
+            if num == candi:
+                cnt += 1
+        if 2 * cnt > len(nums):
+            return candi
+        return -1
 
 if __name__ == '__main__':
     solution = Solution()
-
-    print()
+    nums = [1,2,5,9,5,9,5,5,5]
+    nums = [2,2,1,1,1,2,2]
+    print(solution.majorityElement(nums))
