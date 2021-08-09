ceead34b34625d715c10ec906acc4ae9eb4949ac
lee <aujblee@foxmail.com>
2021-08-09 10:12:54 +0800

    dp

---
+++ dp/ex313.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def nthSuperUglyNumber(self, n: int, primes: List[int]) -> int:
+        dp = [0] * (n+1)
+        dp[1] = 1
+        len_p = len(primes)
+        p = [1] * len_p
+        for i in range(2, n+1):
+            candidates = [p[i] * primes[i] for i in range(len_p)]
+            min_can, min_idx = candidates[0], 0
+            for j in range(len_p):
+                if candidates[j] <
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution)
