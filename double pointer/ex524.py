dcda7551c3fc47b1777374cc806cdce15d53187d
lee <aujblee@foxmail.com>
2021-09-14 09:26:15 +0800

    double pointers

--- double pointer/ex524.py
+++ double pointer/ex524.py
@@ -1,28 +1,41 @@
 from typing import List
 
 
+# class Solution:
+#     def findLongestWord(self, s: str, dictionary: List[str]) -> str:
+#         def found(ss):
+#             if len(ss) > len(s):
+#                 return False
+#             i = j = 0
+#             while i < len(s) and j < len(ss):
+#                 if s[i] == ss[j]:
+#                     j += 1
+#                 i += 1
+#             return j == len(ss)
+#         res = ''
+#         for dic in dictionary:
+#             if found(dic):
+#                 if len(dic) > len(res):
+#                     res = dic
+#                 elif len(dic) == len(res):
+#                     res = min(res, dic)
+#                 else:
+#                     pass
+#         return res
+
 class Solution:
     def findLongestWord(self, s: str, dictionary: List[str]) -> str:
-        def found(ss):
-            if len(ss) > len(s):
-                return False
-            i = j = 0
-            while i < len(s) and j < len(ss):
-                if s[i] == ss[j]:
-                    j += 1
-                i += 1
-            return j == len(ss)
         res = ''
-        for dic in dictionary:
-            if found(dic):
-                if len(dic) > len(res):
-                    res = dic
-                elif len(dic) == len(res):
-                    res = min(res, dic)
-                else:
-                    pass
+        for ss in dictionary:
+            i = j = 0
+            while i < len(ss) and j < len(s):
+                if ss[i] == s[j]:
+                    i += 1
+                j += 1
+            if i == len(ss):
+                if len(ss) > len(res) or (len(ss) == len(res) and ss < res):
+                    res = ss
         return res
-
 
 
 if __name__ == '__main__':
