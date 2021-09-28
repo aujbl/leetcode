0482b046c176a3ef21d4c1b1ac1172873106f406
lee <aujblee@foxmail.com>
2021-09-28 10:07:18 +0800

    prefix sum + linklist, hard

---
+++ prefix sum/ex437.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: jbl
+@time: 2021/9/28 9:51
+"""
+import collections
+from typing import List
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def pathSum(self, root: TreeNode, targetSum: int) -> int:
+        prefix = collections.defaultdict(int)
+        prefix[0] = 1
+
+        def dfs(root, curr):
+            if not root:
+                return 0
+
+            ret = 0
+            curr += root.val
+            ret += prefix[curr - targetSum]
+            prefix[curr] += 1
+            ret += dfs(root.left, curr)
+            ret += dfs(root.right, curr)
+            prefix[curr] -= 1
+
+            return ret
+        return dfs(root, 0)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    print(solution)
