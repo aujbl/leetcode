bf7b0086c669a41532395cfd3bfdf78435c0be71
aujbl <aujblee@foxmail.com>
2022-01-17 12:35:35 +0800

    dp

---
+++ dp/ex1220.py
@@ -0,0 +1,15 @@
+from curses.ascii import SO
+
+
+class Solution:
+    def countVowelPermutation(self, n: int) -> int:
+        dp = (1, 1, 1, 1, 1)
+        MOD = 10 ** 9 + 7
+        for _ in range(n-1):
+            dp = ((dp[1] + dp[2] + dp[4]) % MOD, (dp[0] + dp[2]) % MOD, (dp[1] + dp[3]) % MOD, dp[2] % MOD, (dp[2] + dp[3]) % MOD)
+        return sum(dp) % MOD
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.countVowelPermutation(5))
